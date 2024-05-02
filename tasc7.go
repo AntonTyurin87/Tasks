ca8cfe0026113834cdcd4f55a7cf69e60b4440c3
Anton-Tyurin-AntonTyurin@gmail.com <AntonTyurin87@gmail.com>
2024-05-02 18:29:09 +0300

    Tasc7.go

--- tasc7.go
+++ tasc7.go
@@ -2,6 +2,86 @@ package main
 
 import (
 	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+type Record struct {
+	index  int
+	window int
+	moved  rune
+}
+
+func main() {
+	var amountTests uint16
+	var amountWindows, amountRecords int
+	input := bufio.NewReader(os.Stdin)
+
+	fmt.Fscanln(input, &amountTests)
+
+	for ; amountTests > 0; amountTests-- {
+		fmt.Fscan(input, &amountWindows, &amountRecords)
+		windows := make([]bool, amountWindows, amountWindows)
+		records := make([]Record, amountRecords, amountRecords)
+
+		for i := range records {
+			records[i] = Record{index: i, moved: '0'}
+			fmt.Fscan(input, &records[i].window)
+			fmt.Println(records[i].window)
+			records[i].window--
+			fmt.Println(records[i].window)
+		}
+
+		if betterAssist(&records, &windows) != nil {
+			fmt.Println("x")
+		} else {
+			outRecords(&records)
+		}
+	}
+}
+
+func betterAssist(pRecords *[]Record, pWindows *[]bool) error {
+	records := *pRecords
+	windows := *pWindows
+
+	sort.Slice(records, func(i, j int) bool { return records[i].window < records[j].window })
+
+	for i := 0; i < len(records); i++ {
+		index := records[i].window
+		switch {
+		case index > 0 && !windows[index-1]:
+			windows[index-1] = true
+			records[i].moved = '-'
+		case !windows[index]:
+			windows[index] = true
+		case index < len(windows)-1 && !windows[index+1]:
+			windows[index+1] = true
+			records[i].moved = '+'
+		default:
+			return errors.New("x")
+		}
+	}
+	sort.Slice(records, func(i, j int) bool { return records[i].index < records[j].index })
+	return nil
+}
+
+func outRecords(pRecords *[]Record) {
+	builder := strings.Builder{}
+
+	for _, record := range *pRecords {
+		builder.WriteByte(byte(record.moved))
+	}
+	fmt.Println(builder.String())
+}
+
+/*
+package main
+
+import (
+	"bufio"
 	"fmt"
 	"os"
 	"sort"
@@ -39,13 +119,13 @@ func main() {
 		var placeAndIndexNumberSlice [][]int
 
 		fmt.Fscan(file, &countPlaces, &countVisitors)
-		
-		/*
+
+
 		if countPlaces < countVisitors {
 			result = append[result, "x"]
 			break
 		}
-		*/
+
 
 		for countVisitors > 0 {
 
@@ -74,7 +154,7 @@ func main() {
 			}
 
 		//функция разрешения конфликта
-		
+
 
 
 		}
@@ -84,7 +164,7 @@ func main() {
 	}
 
 
-/*
+
 	fmt.Fscan(file, &countAppointment)
 
 	for i := 0; i < countFriends; i++ {
@@ -135,3 +215,4 @@ func main() {
 		fmt.Println(-1)
 	}
 }
+*/
